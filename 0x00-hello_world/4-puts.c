93b828016d7a41fa3f5e54f8866dc37832f7d38f
Valcharles <valentineogbodo@gmail.com>
2022-04-07 13:12:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * main - entry point
  * return : 0 if exited properly, non zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
